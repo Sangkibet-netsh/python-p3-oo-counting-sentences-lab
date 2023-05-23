66a17c5f91a845a338d4b8fb437f6e654086a981
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-05-23 15:38:56 +0300

    Done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import re
 class MyString:
   def __init__(self, value = ''):
         self._value = value
@@ -22,5 +22,18 @@ class MyString:
   #The endswith() method is a built-in string method in Python that is used to check whether a string ends with a specified suffix or sequence of characters.
   def is_question(self):
         return self._value.endswith("?")
+  
   def is_exclamation(self):
         return self._value.endswith("!")
+  
+  def count_sentences(self):
+        if self.value == '':
+            return 0
+        else:
+            return len(re.split(r'[!\?\.](?= )', self.value))
+        
+
+# The 're' module is the regular expression module in 
+# Python, which provides functions for working with 
+# regular expressions. Regular expressions are powerful 
+# tools for pattern matching and string manipulation.
