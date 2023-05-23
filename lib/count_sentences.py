760e7b0652bff39558863ed26b75fe0bacbc2486
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-05-23 15:12:41 +0300

    The myString class  verifies that the value is a string before assigning it.

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value = ''):
+        self._value = value
+        
+  def get_value(self):
+      return self._value
+
+  def set_value(self, value):
+      if (type(value) == str):
+          print(f"Setting value to {value}")
+          self._value = value
+      else:
+          print("The value must be a string.")
+
+  value = property(get_value, set_value)
